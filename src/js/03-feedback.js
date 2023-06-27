216369436a052863a69d456995d62aeb4e80c261
jakunowa <117861625+jakunowa@users.noreply.github.com>
2023-06-27 21:00:56 +0200

    update task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,24 @@
 import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('input[type="email"]');
+const messageEl = document.querySelector('input[name="message"]');
+const btnEl=document.querySelector('button[type="submit"]')
 const LOCAL_KEY = 'feedback-form-state';
 
-console.log(formEl)
+// const updateForm = JSON.parse(localStorage.getItem(LOCAL_KEY));
+const { email, message } = formEl.elements;
+
+function inputData(e) {
+    dataForm = { email: email.value, message: message.value };
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(dataForm));
+  }
+
+  
+  function formSubmit(e) {
+    e.preventDefault();
+    console.log({ email: email.value, message: message.value });
+    e.currentTarget.reset()}
+
+    formEl.addEventListener('input', throttle(inputData, 500));
+formEl.addEventListener('submit', formSubmit);
