08e1cc9f15bff05d78e5aa998d0cc1517586c1de
jakunowa <117861625+jakunowa@users.noreply.github.com>
2023-07-01 08:36:32 +0000

    Deploying to gh-pages from @ jakunowa/goit-js-hw-08@4d6589373cd5a0f13fc33fc0a3fda44e6ced7059 🚀

--- 02-video.a74b541c.js
+++ 02-video.a74b541c.js
@@ -585,7 +585,7 @@ const getDurationTime = (data)=>{
     localStorage.setItem("videoplayer-current-time", data.seconds);
 };
 const saveTimeDelay = (0, _lodashThrottleDefault.default)(getDurationTime, 1000);
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then();
 player.on("timeupdate", saveTimeDelay);
 
 },{"@vimeo/player":"kmmUG","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","lodash.throttle":"bGJVT"}],"kmmUG":[function(require,module,exports) {
